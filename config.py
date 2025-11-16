de5cf59d99150ba2626cacf47fecd68f97e16527
Alexander8841 <alexandrov327027@yandex.ru>
2025-11-17 01:11:40 +0300

    add cert

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
-SSL = os.getenv("SSL")
+SSL="/certs/root.crt"
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 # Формируем строку подключения к MySQL
